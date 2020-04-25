8677226a9991682a1ea727575f170badb07dbdb0
砖农阿星 <1252619059@qq.com>
2020-04-25 18:51:28 +0800

    Xmall后台管理系统11

---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="dashboard-container">
+    <div class="dashboard-text">欢迎：{{ name }}</div>
+    <div class="dashboard-text">角色：<span v-for="role in roles" :key="role">{{ role }}</span></div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'Dashboard',
+  computed: {
+    ...mapGetters([
+      'name',
+      'roles'
+    ])
+  }
+}
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+.dashboard {
+  &-container {
+    margin: 100px;
+  }
+  &-text {
+    font-size: 50px;
+    line-height: 50px;
+  }
+}
+</style>
